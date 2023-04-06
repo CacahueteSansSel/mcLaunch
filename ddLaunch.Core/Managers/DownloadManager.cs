07eac0094ccdb82d48e74180c8db9d330e1cc86f
Cacahuète <24205659+CacahueteSansSel@users.noreply.github.com>
2023-04-06 23:13:30 +0200

    core: new download system (BROKE THINGS)

--- ddLaunch.Core/Managers/DownloadManager.cs
+++ ddLaunch.Core/Managers/DownloadManager.cs
@@ -3,6 +3,7 @@ using System.IO.Compression;
 using System.Net.Http.Headers;
 using Cacahuete.MinecraftLib.Core;
 using Cacahuete.MinecraftLib.Download;
+using System;
 
 namespace ddLaunch.Core.Managers;
 
@@ -10,11 +11,11 @@ public static class DownloadManager
 {
     static string currentSectionName;
     static List<DownloadEntry> currentSectionEntries = new();
-    
+
     static List<DownloadSection> sections = new();
-    
+
     static HttpClient client;
-    
+
     public static DownloadSection? CurrentSection { get; private set; }
     public static int PendingSectionCount => sections.Count;
     public static string DescriptionLine => CurrentSection == null ? "No pending download" : CurrentSection.Name;
@@ -40,11 +41,12 @@ public static class DownloadManager
             throw new InvalidOperationException($"Last download event '{currentSectionName}' is not finished");
             return;
         }
+
         currentSectionName = name;
 
         client = new HttpClient();
         client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ddLaunch", "1.0.0"));
-        
+
         OnDownloadPrepareStarting?.Invoke(name);
     }
 
@@ -66,7 +68,7 @@ public static class DownloadManager
     {
         await Task.Run(async () =>
         {
-            while (IsDownloadInProgress) 
+            while (IsDownloadInProgress)
                 await Task.Delay(1);
         });
     }
@@ -80,13 +82,13 @@ public static class DownloadManager
     {
         if (IsDownloadInProgress) return;
         IsDownloadInProgress = true;
-        
+
         int sectionIndex = 0;
         foreach (DownloadSection section in sections)
         {
             CurrentSection = section;
             OnDownloadSectionStarting?.Invoke(section.Name, sectionIndex);
-            
+
             int progress = 0;
             foreach (DownloadEntry entry in section.Entries)
             {
@@ -95,40 +97,66 @@ public static class DownloadManager
                     case EntryAction.Download:
                         try
                         {
-                            HttpResponseMessage resp = await client.GetAsync(entry.Source);
+                            HttpResponseMessage resp = await client.GetAsync(entry.Source,
+                                HttpCompletionOption.ResponseHeadersRead);
                             resp.EnsureSuccessStatusCode();
 
                             string folder = entry.Target.Replace(Path.GetFileName(entry.Target), "").Trim('/');
                             if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
 
-                            await File.WriteAllBytesAsync(entry.Target, await resp.Content.ReadAsByteArrayAsync());
+                            Stream downloadStream = await resp.Content.ReadAsStreamAsync();
+                            long size = resp.Content.Headers.ContentLength ?? 0;
+                            FileStream fs = new FileStream(entry.Target, FileMode.Create, FileAccess.Write);
+                            long b = 0;
+
+                            while (true)
+                            {
+                                float oneEntryMax = 1f / section.Entries.Count;
+                                double byteProgress = (double) b / size * oneEntryMax;
+
+                                byte[] buffer = new byte[25];
+                                int input = await downloadStream.ReadAsync(buffer);
+                                if (input == 0) break;
+
+                                await fs.WriteAsync(buffer, 0, input);
+
+                                OnDownloadProgressUpdate?.Invoke(entry.Source,
+                                    (float) progress / section.Entries.Count + (float) byteProgress,
+                                    sectionIndex);
+
+                                b += 25;
+                            }
+
+                            fs.Close();
                         }
-                        catch (Exception e)
+                        catch (InvalidProgramException e)
                         {
                             OnDownloadError?.Invoke(section.Name, entry.Source);
                         }
+
                         break;
                     case EntryAction.Extract:
                         if (!Directory.Exists(entry.Target)) Directory.CreateDirectory(entry.Target);
-                        
+
                         ZipFile.ExtractToDirectory(entry.Source, entry.Target, true);
                         break;
                 }
 
                 progress++;
-                OnDownloadProgressUpdate?.Invoke(entry.Source, (float) progress / currentSectionEntries.Count, sectionIndex);
+                OnDownloadProgressUpdate?.Invoke(entry.Source, (float) progress / section.Entries.Count,
+                    sectionIndex);
             }
 
             sectionIndex++;
         }
-        
+
         CurrentSection = null;
-        
+
         IsDownloadInProgress = false;
         sections.Clear();
         currentSectionEntries.Clear();
         OnDownloadFinished?.Invoke();
-        
+
         Debug.WriteLine("Download Jobs Finished");
     }
 
