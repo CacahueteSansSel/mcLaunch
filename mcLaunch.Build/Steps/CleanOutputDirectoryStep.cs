0094de39209037da8efb53e8a8773a57d48a90cf
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2024-03-01 23:19:31 +0100

    build: added clean output directory build step

--- mcLaunch.Build/Steps/CleanOutputDirectoryStep.cs
+++ mcLaunch.Build/Steps/CleanOutputDirectoryStep.cs
@@ -7,7 +7,7 @@ public class CleanOutputDirectoryStep : BuildStepBase
     public override async Task<BuildResult> RunAsync(BuildSystem system)
     {
         if (Directory.Exists($"{system.SolutionDirectory}/output"))
-            Directory.Delete($"{system.SolutionDirectory}/output");
+            Directory.Delete($"{system.SolutionDirectory}/output", true);
 
         return new BuildResult();
     }
